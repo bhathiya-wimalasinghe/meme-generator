9fff02d3b8de6e22e43d30edf2198685a3b46230
bhathiya-wimalasinghe <bhathiyawimalasinghe@gmail.com>
2024-02-28 15:08:59 +0530

    getMemeImage() was implemented

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,6 +1,16 @@
 import React from "react";
+import memesData from "../memesData";
 
 export default function Meme() {
+  const [memeImage, setMemeImage] = React.useState("");
+
+  function getMemeImage() {
+    const memeArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memeArray.length);
+
+    setMemeImage(memeArray[randomNumber].url);
+  }
+
   return (
     <main>
       <div className="form">
@@ -10,8 +20,11 @@ export default function Meme() {
           type="text"
           placeholder="and take my money"
         />
-        <button className="form--button">Get a new meme image 🖼</button>
+        <button onClick={getMemeImage} className="form--button">
+          Get a new meme image 🖼
+        </button>
       </div>
+      <img src={memeImage} className="meme--image" />
     </main>
   );
 }
